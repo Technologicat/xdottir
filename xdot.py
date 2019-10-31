d58cbf70f12511cabaf77cf6f73c7e04d6c1620a
Juha Jeronen <juha.jeronen@gbw.fi>
2019-10-31 10:35:51 +0200

    Add missing gi requires

--- xdot.py
+++ xdot.py
@@ -48,6 +48,8 @@ from itertools import chain
 
 import gi
 gi.require_version('Gtk', '3.0')
+gi.require_version('Gdk', '3.0')
+gi.require_version('GdkPixbuf', '2.0')
 gi.require_version('Pango', '1.0')
 gi.require_version('PangoCairo', '1.0')
 
